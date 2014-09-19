68b7ba211369e1fdde69daba7620326fdffe4c34
Kartik Kumar <me@kartikkumar.com>
2014-09-19 13:10:12 +0200

    Replace CMake module with script for generic SGP4 library. Remove version file.

---
+++ FindSGP4.cmake
@@ -0,0 +1,33 @@
+# Copyright (c) 2014, K. Kumar (me@kartikkumar.com)
+# All rights reserved.
+
+if (SGP4_INCLUDE_DIRS)
+  # in cache already
+  set(SGP4_FOUND TRUE)
+else (SGP4_INCLUDE_DIRS)
+
+  find_path(SGP4_INCLUDE_DIR
+    NAMES
+      SGP4-VER.TLE
+    PATHS
+      /usr/include
+      /usr/local/include
+      /opt/local/include
+      /sw/include
+      /usr/local
+      ${PROJECT_ROOT}
+      ${PROJECT_ROOT}/..      
+      PATH_SUFFIXES sgp4
+  )
+
+  set(SGP4_INCLUDE_DIRS
+    ${SGP4_INCLUDE_DIR}
+  )
+
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(SGP4 DEFAULT_MSG SGP4_INCLUDE_DIRS)
+
+  # show the SGP4_INCLUDE_DIRS variables only in the advanced view
+  mark_as_advanced(SGP4_INCLUDE_DIRS)
+
+endif (SGP4_INCLUDE_DIRS)
